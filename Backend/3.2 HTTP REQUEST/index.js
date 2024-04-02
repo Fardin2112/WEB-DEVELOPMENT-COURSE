c795e490de015d06c428780197c8bb99d58a380a
Fardin2112 <fardinalam2112@gmail.com>
2024-04-03 00:16:31 +0530

    learn postman again

--- Backend/3.2 HTTP REQUEST/index.js
+++ Backend/3.2 HTTP REQUEST/index.js
@@ -7,6 +7,10 @@ app.get("/",(req,res)=>{
     res.send("hey bro i am working from responce")
 })
 
+app.get("/about",(req,res)=>{
+    res.send("hey bro i am working from responce from about ")
+})
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}.`)
 })
